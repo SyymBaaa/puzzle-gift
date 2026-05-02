ead6d343712c2c102b1c901f448a818691335777
SyymBaaa <96062142+SyymBaaa@users.noreply.github.com>
2026-05-03 00:22:48 +0300

    Port 8000

--- main.py
+++ main.py
@@ -484,5 +484,6 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
+    import os
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
